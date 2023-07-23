3e9d520965ab177f814c7d626f5e9a16d1e65b32
fredymar <fredymartinezquinto@hotmail.com>
2023-07-22 19:58:21 -0500

    ejercicio

--- Javascript/04-String/Ejercicio/ejercicio.js
+++ Javascript/04-String/Ejercicio/ejercicio.js
@@ -15,17 +15,21 @@ console.log(estudianteLength);
 
 // Todas válidas
 // const inicialNombre = nombre[0]
+
 // const inicialNombre = nombre.at(0)
 const inicialNombre = nombre.substring(0, 1)
 
 console.log(inicialNombre);
 // Todas válidas
+
 // const finalApellido = apellido[apellido.length - 1]
+
 // const finalApellido = nombre.at(apellido.length - 1)
 const finalApellido = apellido.substring(apellido.length - 1, apellido.length)
 console.log(finalApellido);
 
 // Todas válidas
+
 // const estudianteSinEspacios = estudiante.replace(" ", "")
 const estudianteSinEspacios = estudiante.replace(/ /g, "")
 console.log(estudianteSinEspacios);
